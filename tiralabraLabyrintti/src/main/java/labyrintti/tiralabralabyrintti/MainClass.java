7f220deff7b8f9a9d376093b8b81aaa1eec250d2
Matias Himanen <matias.himanen@helsinki.fi>
2020-10-01 19:39:46 +0300

    -

--- tiralabraLabyrintti/src/main/java/labyrintti/tiralabralabyrintti/MainClass.java
+++ tiralabraLabyrintti/src/main/java/labyrintti/tiralabralabyrintti/MainClass.java
@@ -15,27 +15,7 @@ LabyrinttienVertailu lv = new LabyrinttienVertailu();
 lv.vertaa(100);
 
 
-Pino p = new Pino(5);
 
-//p.lisaa(1);
-//p.lisaa(2);
-//p.lisaa(3);
-//p.lisaa(4);
-//p.lisaa(5);
-//
-//while(!p.onkoTyhja()){
-//    System.out.println(p.PalautaEka());
-//}
-//
-//
-//
-//p.lisaa(3);
-//p.lisaa(1);
-//p.lisaa(2);
-//
-//while(!p.onkoTyhja()){
-//    System.out.println(p.PalautaEka());
-//}
 
     }
 }
